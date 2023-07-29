e91e4a667ab2cd3ec808208baa69993b10866f6b
Prasetyo Nur Hidayat <prasetyonurhidayat@gmail.com>
2023-07-29 09:13:58 +0700

    init

--- src/services/API.jsx
+++ src/services/API.jsx
@@ -1,16 +1,14 @@
-import axios from "axios";
+import axios from 'axios';
 
 export const API = axios.create({
-    baseURL: "http://localhost:8080/api/v1/",
-    withCredentials: true
+  baseURL: 'http://192.53.117.185:3001/api/v1/',
+  withCredentials: true
 });
 
-
 export const setAuthToken = (token) => {
-    if (token) {
-        API.defaults.headers.common['Authorization'] = `Bearer ${token}`;
-    } else {
-        delete API.defaults.headers.common['Authorization'];
-    }
-}
-
+  if (token) {
+    API.defaults.headers.common['Authorization'] = `Bearer ${token}`;
+  } else {
+    delete API.defaults.headers.common['Authorization'];
+  }
+};
