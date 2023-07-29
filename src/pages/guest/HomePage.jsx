4f118f5daefc50d278fe8ac5db509e4c845991ea
Prasetyo Nur Hidayat <prasetyonurhidayat@gmail.com>
2023-07-29 07:05:48 +0700

    first commit

--- src/pages/guest/HomePage.jsx
+++ src/pages/guest/HomePage.jsx
@@ -1,24 +1,23 @@
-import React from 'react'
+import React from 'react';
 
-import HomePage from '../../components/layout/HomePage'
-import { products } from '../../data/Dummy'
-
+import HomePage from '../../components/layout/HomePage';
+import { products } from '../../data/dummy';
 
 export default function Home() {
-    // const [products, setProducts] = useState([])
-    // const getData = async () => {
-    //     const data = await axios.get('http://localhost:8080/api/v1/products')
-    //     const rest = data?.data
-    //     setProducts(rest)
-    // }
-    // useEffect(() => {
-    //     getData()
-    // }, [])
+  // const [products, setProducts] = useState([])
+  // const getData = async () => {
+  //     const data = await axios.get('http://localhost:8080/api/v1/products')
+  //     const rest = data?.data
+  //     setProducts(rest)
+  // }
+  // useEffect(() => {
+  //     getData()
+  // }, [])
 
-    // console.log("data", products);
-    return (
-        <>
-            <HomePage products={products} />
-        </>
-    )
+  // console.log("data", products);
+  return (
+    <>
+      <HomePage products={products} />
+    </>
+  );
 }
